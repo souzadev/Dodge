e5d77d4061ce2d344db99d2903891d9ade065c49
Edson Souza <edson.dev.souza@gmail.com>
2014-05-12 09:39:24 -0300

    Basic Layout

--- gen/com/souzadev/dodge/R.java
+++ gen/com/souzadev/dodge/R.java
@@ -25,20 +25,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int main_relativeLayout_bot=0x7f080001;
-        public static final int main_relativeLayout_top=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int main_button_exit=0x7f080002;
+        public static final int main_button_play=0x7f080000;
+        public static final int main_button_settings=0x7f080001;
+        public static final int main_rLayout_bop=0x7f080004;
+        public static final int main_rLayout_top=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_settings=0x7f030001;
+        public static final int activity_world=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int settings=0x7f070001;
+        public static final int world=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_settings=0x7f050004;
+        public static final int title_activity_world=0x7f050003;
     }
     public static final class style {
         /** 
---
+++ src/com/souzadev/dodge/WorldActivity.java
@@ -0,0 +1,30 @@
+package com.souzadev.dodge;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class WorldActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_world);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.world, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {		
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
